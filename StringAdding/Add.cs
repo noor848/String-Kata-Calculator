bd8dd00b5656d0bc7a28f7ddffbe311cefbf4488
noor848 <54281955+noor848@users.noreply.github.com>
2022-08-13 02:01:22 +0300

    Part One Solution

---
+++ StringAdding/Add.cs
@@ -0,0 +1,21 @@
+﻿namespace StringAdding
+{
+    public class Add
+    {
+        public int CalculatingString(string StringInput)
+        {
+            if (string.IsNullOrEmpty(StringInput))
+            {
+                return 0;
+            }
+            String[] strlist = StringInput.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            int sum = 0;
+            foreach (var str in strlist)
+            {
+                sum += Int16.Parse(str);
+            }
+
+            return sum;
+        }
+    }
+}
