51b391b4475aa70d89f8f1bd76d89223f928c3f7
noor848 <54281955+noor848@users.noreply.github.com>
2022-08-13 02:27:10 +0300

    PART3 more than two integer in the string

--- TestingString/UnitTest1.cs
+++ TestingString/UnitTest1.cs
@@ -5,12 +5,13 @@ namespace TestingString
     public class UnitTest1
     {
         [Theory]
-        [InlineData("")]
+     
+        [InlineData("1,2,2,1")]
         public void Test1(string InputString)
         {
             Add c = new Add();
             int sum = c.CalculatingString(InputString);
-            Assert.Equal(0, sum);
+            Assert.Equal(8, sum);
           
         }
     }
