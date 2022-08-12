a05198121b3057620d9094c99822cf8b0bed1ffd
noor848 <54281955+noor848@users.noreply.github.com>
2022-08-13 02:23:20 +0300

    Empty string

--- TestingString/UnitTest1.cs
+++ TestingString/UnitTest1.cs
@@ -5,12 +5,13 @@ namespace TestingString
     public class UnitTest1
     {
         [Theory]
-        [InlineData("1,2")]
+        [InlineData("")]
         public void Test1(string InputString)
         {
             Add c = new Add();
             int sum = c.CalculatingString(InputString);
-            Assert.Equal(3, sum);
+            Assert.Equal(0, sum);
+          
         }
     }
 }
