eba717eda704a9ca2e41e9fec66c2ecb411db44a
noor848 <54281955+noor848@users.noreply.github.com>
2022-08-13 21:10:31 +0300

    PART3 Refactoring Code

--- TestingString/UnitTest1.cs
+++ TestingString/UnitTest1.cs
@@ -5,12 +5,12 @@ namespace TestingString
     public class UnitTest1
     {
         [Theory]
-        [InlineData("1,2,2,1")]
-        public void Test1(string InputString)
+        [InlineData("1,2,2,1",6)]
+        public void Test1(string InputString,int Actual)
         {
             Add c = new Add();
             int sum = c.CalculatingString(InputString);
-            Assert.Equal(8, sum);
+            Assert.Equal(Actual, sum);
           
         }
     }
