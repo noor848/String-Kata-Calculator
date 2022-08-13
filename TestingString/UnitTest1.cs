f78d2a09a6a1268e1e122597eeea772de196743b
noor848 <54281955+noor848@users.noreply.github.com>
2022-08-13 18:22:24 +0300

    PART4 with \n

--- TestingString/UnitTest1.cs
+++ TestingString/UnitTest1.cs
@@ -5,12 +5,12 @@ namespace TestingString
     public class UnitTest1
     {
         [Theory]
-        [InlineData("1,2,2,1")]
+        [InlineData("1\n2,2,1")]
         public void Test1(string InputString)
         {
             Add c = new Add();
             int sum = c.CalculatingString(InputString);
-            Assert.Equal(8, sum);
+            Assert.Equal(6, sum);
           
         }
     }
