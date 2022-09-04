9692bdbd4292c0a407d0e4f4ede57d57fdd38945
noor848 <54281955+noor848@users.noreply.github.com>
2022-09-04 17:04:47 +0300

    Fixing Code according to comments

--- TestingString/UnitTest1.cs
+++ TestingString/UnitTest1.cs
@@ -9,8 +9,8 @@ namespace TestingString
     
         public void Test1(string InputString,int Actual)
         {
-            Add c = new Add();
-            int sum = c.CalculatingStringFirstSolution(InputString);
+            AddingString c = new AddingString();
+            int sum = c.Add1(InputString);
             Assert.Equal(sum, Actual);
         }
 
@@ -18,16 +18,16 @@ namespace TestingString
         [InlineData("1,2", 3)]
         public void Test3(string InputString, int Actual)
         {
-            Add c = new Add();
-            int sum = c.CalculatingStringFirstSolution(InputString);
+            AddingString c = new AddingString();
+            int sum = c.Add1(InputString);
             Assert.Equal(sum, Actual);
         }
 
         [InlineData("1", 1)]
         public void Test4(string InputString, int Actual)
         {
-            Add c = new Add();
-            int sum = c.CalculatingStringFirstSolution(InputString);
+            AddingString c = new AddingString();
+            int sum = c.Add1(InputString);
             Assert.Equal(sum, Actual);
         }
 
@@ -35,24 +35,24 @@ namespace TestingString
         [InlineData("1,2,3,4,5", 15)]
         public void Test5(string InputString, int Actual)
         {
-            Add c = new Add();
-            int sum = c.CalculatingStringSecondSolution(InputString);
+            AddingString c = new AddingString();
+            int sum = c.Add2(InputString);
             Assert.Equal(sum, Actual);
         }
 
         [InlineData("1\n2,3", 6)]
         public void Test6(string InputString, int Actual)
         {
-            Add c = new Add();
-            int sum = c.CalculatingStringThirdSolution(InputString);
+            AddingString c = new AddingString();
+            int sum = c.Add3(InputString);
             Assert.Equal(sum, Actual);
         }
 
         [InlineData("//;\n1;2", 6)]
         public void Test7(string InputString, int Actual)
         {
-            Add c = new Add();
-            int sum = c.CalculatingStringFourthSolution(InputString);
+            AddingString c = new AddingString();
+            int sum = c.Add4(InputString);
             Assert.Equal(sum, Actual);
         }
 
@@ -61,9 +61,8 @@ namespace TestingString
         [InlineData("//;\n1;-2,-4", "-2,-4")]
         public void Test8(string InputString, string Actual)
         {
-            Add c = new Add();
-            int sum = c.CalculatingString(InputString);
-            Action act = () => c.CalculatingString(InputString);
+            AddingString c = new AddingString();
+            Action act = () => c.Add5(InputString);
             ArgumentException exception = Assert.Throws<ArgumentException>(act);
             Assert.Equal("Negative Number" + Actual, exception.Message);
         }
@@ -72,8 +71,8 @@ namespace TestingString
         [InlineData("1,2,1001", 3)]
         public void Test9(string InputString, int Actual)
         {
-            Add c = new Add();
-            int sum = c.CalculatingString(InputString);
+            AddingString c = new AddingString();
+            int sum = c.Add6(InputString);
             Assert.Equal(sum, Actual);
         }
 
