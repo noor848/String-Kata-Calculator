5a4cceb005f51405c62791e6570eac9714075536
noor848 <54281955+noor848@users.noreply.github.com>
2022-09-04 18:38:56 +0300

    Refactoring the code according to instructions

--- TestingString/UnitTest1.cs
+++ TestingString/UnitTest1.cs
@@ -6,60 +6,38 @@ namespace TestingString
     {
         [Theory]
         [InlineData("",0)]
-    
-        public void Test1(string InputString,int Actual)
-        {
-            AddingString c = new AddingString();
-            int sum = c.Add1(InputString);
-            Assert.Equal(sum, Actual);
-        }
-
-
         [InlineData("1,2", 3)]
-        public void Test3(string InputString, int Actual)
-        {
-            AddingString c = new AddingString();
-            int sum = c.Add1(InputString);
-            Assert.Equal(sum, Actual);
-        }
-
         [InlineData("1", 1)]
-        public void Test4(string InputString, int Actual)
+        [InlineData("1,2,3,4,5", 15)]
+        public void TestStringWithCommaSeperated(string InputString,int Actual)
         {
             AddingString c = new AddingString();
             int sum = c.Add1(InputString);
             Assert.Equal(sum, Actual);
         }
 
-
-        [InlineData("1,2,3,4,5", 15)]
-        public void Test5(string InputString, int Actual)
-        {
-            AddingString c = new AddingString();
-            int sum = c.Add2(InputString);
-            Assert.Equal(sum, Actual);
-        }
-
+        [Theory]
         [InlineData("1\n2,3", 6)]
-        public void Test6(string InputString, int Actual)
+        public void TestStringwithNewLineSeperated(string InputString, int Actual)
         {
             AddingString c = new AddingString();
             int sum = c.Add3(InputString);
             Assert.Equal(sum, Actual);
         }
 
+        [Theory]
         [InlineData("//;\n1;2", 6)]
-        public void Test7(string InputString, int Actual)
+        public void TestStringwithDifferentTypeOfDelimeterSeperated(string InputString, int Actual)
         {
             AddingString c = new AddingString();
             int sum = c.Add4(InputString);
             Assert.Equal(sum, Actual);
         }
 
-
+        [Theory]
         [InlineData("1,2,-1", "-1")]
         [InlineData("//;\n1;-2,-4", "-2,-4")]
-        public void Test8(string InputString, string Actual)
+        public void TestingWithNegativeNumber(string InputString, string Actual)
         {
             AddingString c = new AddingString();
             Action act = () => c.Add5(InputString);
@@ -67,9 +45,9 @@ namespace TestingString
             Assert.Equal("Negative Number" + Actual, exception.Message);
         }
 
-
+        [Theory]
         [InlineData("1,2,1001", 3)]
-        public void Test9(string InputString, int Actual)
+        public void TestExcludingRangeOfNumber(string InputString, int Actual)
         {
             AddingString c = new AddingString();
             int sum = c.Add6(InputString);
