248d05faf4ba3093596aeb72ae8d120ffd9586fb
noor848 <54281955+noor848@users.noreply.github.com>
2022-09-05 18:19:00 +0300

    Fixing Code issues

---
+++ StringAdding/AddingString.cs
@@ -0,0 +1,50 @@
+﻿namespace StringAdding
+{
+    public class AddingString
+    {
+        public const int UpperLimit = 1000;
+        public int Add(string StringInput)
+        {
+            int sum = 0;
+            List<int> NumbersList;
+            if (string.IsNullOrEmpty(StringInput))
+            {
+                return 0;
+            }
+
+            if (StringInput.StartsWith("//"))
+            {
+                ///"//; \n 1;2" 
+                var SplitString = StringInput.Split(new char[] { '\n' },
+                 StringInput.Length,StringSplitOptions.RemoveEmptyEntries);
+                var BackSepration = SplitString[0].Trim('/');
+                NumbersList = SplitString[1].Split(BackSepration, StringInput.Length, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse)?.ToList();
+            }
+            else
+            {
+                NumbersList = StringInput.Split(new char[] { ',', '\n', ';' }, StringInput.Length,StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse)?.ToList();
+            }
+
+            List<int> NegativeNumbers = new List<int>();
+            foreach (var num in NumbersList)
+            {
+                if (num < 0)
+                {
+                    NegativeNumbers.Add(num);
+                }
+                else if (num <= 1000)
+                {
+                    sum += num;
+                }
+            }
+
+            if (NegativeNumbers.Any())
+            {
+                throw new Exception("Negative Number" + String.Join(",", NegativeNumbers));
+            }
+
+            return sum;
+        }
+
+    }
+}
--- TestingString/UnitTest1.cs
+++ TestingString/UnitTest1.cs
@@ -1,59 +1,33 @@
-
 using StringAdding;
+using Xunit;
 namespace TestingString
 {
     public class UnitTest1
     {
-        [Theory]
-        [InlineData("",0)]
-        [InlineData("1,2", 3)]
-        [InlineData("1", 1)]
-        [InlineData("1,2,3,4,5", 15)]
-        public void TestStringWithCommaSeperated(string InputString,int Actual)
-        {
-            AddingString c = new AddingString();
-            int sum = c.Add1(InputString);
-            Assert.Equal(sum, Actual);
+            [Theory]
+            [InlineData("", 0)]
+            [InlineData("1,2", 3)]
+            [InlineData("1", 1)]
+            [InlineData("1,2,3,4,5", 15)]
+            [InlineData("1\n2,3", 6)]
+            [InlineData("//;\n1;2", 3)]
+            [InlineData("1,2,1001", 3)]
+            public void TestStringWithDifferentDelimeterSeperated(string InputString, int Actual)
+            {
+                AddingString c = new AddingString();
+                int sum = c.Add(InputString);
+                Assert.Equal(sum, Actual);
+            }
+
+            [Theory]
+            [InlineData("1,2,-1", "-1")]
+            public void TestingWithNegativeNumber(string InputString, string Actual)
+            {
+                AddingString c = new AddingString();
+                Action act = () => c.Add(InputString);
+                Exception exception = Assert.Throws<Exception>(act);
+                Assert.Equal($"Negative Number{Actual}", exception.Message);
+            }
+
         }
-
-        [Theory]
-        [InlineData("1\n2,3", 6)]
-        public void TestStringwithNewLineSeperated(string InputString, int Actual)
-        {
-            AddingString c = new AddingString();
-            int sum = c.Add3(InputString);
-            Assert.Equal(sum, Actual);
-        }
-
-        [Theory]
-        [InlineData("//;\n1;2", 6)]
-        public void TestStringwithDifferentTypeOfDelimeterSeperated(string InputString, int Actual)
-        {
-            AddingString c = new AddingString();
-            int sum = c.Add4(InputString);
-            Assert.Equal(sum, Actual);
-        }
-
-        [Theory]
-        [InlineData("1,2,-1", "-1")]
-        [InlineData("//;\n1;-2,-4", "-2,-4")]
-        public void TestingWithNegativeNumber(string InputString, string Actual)
-        {
-            AddingString c = new AddingString();
-            Action act = () => c.Add5(InputString);
-            ArgumentException exception = Assert.Throws<ArgumentException>(act);
-            Assert.Equal("Negative Number" + Actual, exception.Message);
-        }
-
-        [Theory]
-        [InlineData("1,2,1001", 3)]
-        public void TestExcludingRangeOfNumber(string InputString, int Actual)
-        {
-            AddingString c = new AddingString();
-            int sum = c.Add6(InputString);
-            Assert.Equal(sum, Actual);
-        }
-
-
     }
-}
